40976ad819609c0b5e02349402b7209f10ff5e71
BM-WBingman <18242092616@163.com>
2018-08-29 09:16:29 +0800

    今天最后一次上传一次GitHub

---
+++ film/classify/classify.js
@@ -0,0 +1,24 @@
+// classify/classify.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    movieList:Array
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+  
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
